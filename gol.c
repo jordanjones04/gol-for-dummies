3307d0807c6e026fe2119ac0741785a3d36959db
jordanjones04 <131913935+jordanjones04@users.noreply.github.com>
2023-04-27 08:46:15 -0400

    Update gol.c

--- gol.c
+++ gol.c
@@ -47,6 +47,7 @@ struct timeval start_time, stop_time;
 static int rt = 0;
 static int sr_count = 0;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /* This struct represents all the data you need to keep track of your GOL
  * simulation.  Rather than passing individual arguments into each function,
@@ -64,11 +65,15 @@ struct gol_data {
     int num_alive_cells;
     int* current;
     int* next;
+    int rounds;
     int num_threads;
     int para_mode;
     int print_info;
+    pthread_t thread_id;
     int rows_per_thread;
-    int starting_row;
+    int start_row;
+    int end_row;
+    int **row_partition_info;
     /* fields used by ParaVis library (when run in OUTPUT_VISI mode). */
     visi_handle handle;
     color3 *image_buff;
@@ -80,7 +85,7 @@ struct gol_data {
 void* play_gol(void* args);
 
 /* init gol data from the input file and run mode cmdline args */
-int init_game_data_from_args(struct gol_data *data, char **argv);
+int init_game_data_from_args(struct gol_data *data, int argc, char **argv);
 
 // A mostly implemented function, but a bit more for you to add.
 /* print board to the terminal (for OUTPUT_ASCII mode) */
@@ -97,6 +102,10 @@ void make_alive(struct gol_data *data, int x_axis, int y_axis, int alive);
 
 // changes the colors for the VISI visual output
 void update_colors(struct gol_data* data);
+
+void* play_gol_thread(void* arg);
+int** row_partition(int rows, int cols, int num_threads);
+int** col_partition(int rows, int cols, int num_threads);
 
 /************ Definitions for using ParVisi library ***********/
 /* initialization for the ParaVisi library (DO NOT MODIFY) */
@@ -123,12 +132,12 @@ int main(int argc, char **argv) {
 
     /* Initialize game state (all fields in data) from information
      * read from input file */
-    ret = init_game_data_from_args(&data, argv);
+    ret = init_game_data_from_args(&data, argc, argv);
     if (ret != 0) {
         printf("Initialization error: file %s, mode %s\n", argv[1], argv[2]);
         exit(1);
     }
-     //make the threads
+     //make the threads.
     pthread_t *tid;
     int r;
     int threads = data.num_threads;
@@ -159,15 +168,21 @@ int main(int argc, char **argv) {
     /* Invoke play_gol in different ways based on the run mode */
     if (data.output_mode == OUTPUT_NONE) {  // run with no animation
         //play_gol(&data);
-        int i = 0;
-        int r = pthread_create(&tid[0], NULL, play_gol, &tid_args[i]);
-
+        for(int i = 0; i<data.num_threads;i++){    \
+            int r = pthread_create(&tid[i], NULL, play_gol_thread, &tid_args[i]);
+        }for(int i = 0; i<data.num_threads;i++){
+            int r = pthread_join(tid[i], NULL);
+        }
     }
     else if (data.output_mode == OUTPUT_ASCII) { // run with ascii animation
         //play_gol(&data);
-        int i =0;
-        int r = pthread_create(&tid[0], NULL, play_gol, &tid_args[i]);
-
+        int i;
+        for(i = 0; i<data.num_threads;i++){    \
+            int r = pthread_create(&tid[i], NULL, play_gol_thread, &tid_args[i]);
+        }   //if r ==1 print error message             dklgj;jsdljfklsjkljdsklfjklsjdklfjsklfjklsjkdlfjsdkl
+        for(i = 0; i<data.num_threads;i++){
+            int r = pthread_join(tid[i], NULL);
+        }//if r ==1 print error message                     jkfdjglkjkldjfdslkikjkdflsjskdljfkdljkljsdfkjafkljlkfjklsdwjkdsjkl
     }
     else {  
         // OUTPUT_VISI: run with ParaVisi animation
@@ -220,9 +235,15 @@ int main(int argc, char **argv) {
  *   argv: pointer to an array that contains data of type char and it 
  *         contains the inputs of the user
  */
-int init_game_data_from_args(struct gol_data *data, char **argv) {
+int init_game_data_from_args(struct gol_data *data, int argc, char **argv) {
     FILE *file;
     int rows, cols, num_alive_cells, x, y, num_threads;
+
+    //Check that the correct number of command line arguments are provided
+    if(argc != 6){
+     printf("Usage: %s <input_file> <output_mode> <num_threads> <parallelization_mode> <print_info>\n", argv[0]);
+     return 1;
+    }
 
     // opens the file
     file = fopen(argv[1], "r");
@@ -231,7 +252,7 @@ int init_game_data_from_args(struct gol_data *data, char **argv) {
         exit(1);
     }
 
-    // Sets output_mode to a variable
+    // Sets output_mode from the command line arguments to a variable
     data->output_mode = atoi(argv[2]);
     if(data->output_mode < 0 || data->output_mode > 2){
         printf("Error incombatibale output_mode. Put 0, 1, or 2\n");
@@ -239,10 +260,15 @@ int init_game_data_from_args(struct gol_data *data, char **argv) {
     }
     //Reads in the number of rows, colums, number of alive cells from file
     int ret = fscanf(file, "%d %d %d %d", &data->rows, &data->cols, &data->iters, &data->num_alive_cells);
+    if(ret != 4){
+        printf("Error: improper file format.\n");
+        exit(1);
+    }
     
     //gets the number of threads from command line and save it to
     //a variable.
     data->num_threads = atoi(argv[3]);
+    printf("%d\n", data->num_threads);              //for debugging purposes >.......hjsdfkhsjkhjdkfzhfjkeshbsd
 
     //gets the parallelization mode
     data->para_mode = atoi(argv[4]);
@@ -250,12 +276,6 @@ int init_game_data_from_args(struct gol_data *data, char **argv) {
     //gets the decision on whether or not the thread allocation is printed
     data->print_info = atoi(argv[5]);
     
-
-
-    if(ret != 4){
-        printf("Error Improper file format.\n");
-        exit(1);
-    }
 
     // sets the data from the struct to variables
     num_alive_cells = data->num_alive_cells;
@@ -270,18 +290,15 @@ int init_game_data_from_args(struct gol_data *data, char **argv) {
         printf("ERROR: malloc failed!\n");
         exit(1);
     }
-
-    //make the current board 
-    make_board(data->current, rows, cols);
-
-    //Make a board set equal to dead and then go through and place the cells that are alive
-    data->next = malloc(sizeof(int) * rows * cols);
+     make_board(data->current, rows, cols);
+    
+    data->row_partition_info = row_partition(data->rows, data->cols, data->num_threads); 
+   //make the next board 
+    data->next = malloc(sizeof(int)*rows*cols);
     if (data->next == NULL){
         printf("ERROR: malloc failed!\n");
         exit(1);
     }
-
-   //make the next board 
     make_board(data->next, rows, cols);
 
     for (int i = 0; i < num_alive_cells; i++){
@@ -296,14 +313,53 @@ int init_game_data_from_args(struct gol_data *data, char **argv) {
 
     //close the file when done with it
     ret = fclose(file);
-    if(ret != 0){
-        printf("Error: failed to close file: %s/n", argv[1]);
-        exit(1);
-    }
-
+    // if(ret != 0){ 
+    //      for(int j = 0; j < cols; j++){
+    //          arr[i*cols + j] = 0;
+    //      }
+    // }
     return 0;
 }
 
+int** row_partition(int rows, int cols, int num_threads){ //double free in main!!
+    int ** partition_info = (int**)malloc(num_threads * sizeof(int*));
+    int cells_per_thread = rows / num_threads;
+    int extra_cells = rows % num_threads;
+    int start_row = 0;
+
+    for(int i = 0; i < num_threads; i++){
+        int end_row = start_row + cells_per_thread -1;
+        if(extra_cells > 0){
+            end_row++;
+            extra_cells--;
+        }
+        partition_info[i] = (int*)malloc(2 * sizeof(int));
+        partition_info[i][0] = start_row;
+        partition_info[i][1] = end_row;
+        start_row = end_row + 1;
+    }
+    return partition_info;
+}
+
+int** col_partition(int rows, int cols, int num_threads){ //double free in main!!
+    int ** partition_info = (int**)malloc(num_threads * sizeof(int*));
+    int cells_per_thread = cols / num_threads;
+    int extra_cells = cols % num_threads;
+    int start_col = 0;
+
+    for(int i = 0; i < num_threads; i++){
+        int end_col = start_col + cells_per_thread -1;
+        if(extra_cells > 0){
+            end_col++;
+            extra_cells--;
+        }
+        partition_info[i] = (int*)malloc(2 * sizeof(int));
+        partition_info[i][0] = start_col;
+        partition_info[i][0] = end_col;
+        start_col = end_col + 1;
+    }
+    return partition_info;
+}
 /* helper function to initialize the array that makes up the board
  * arr: is the array of type int
  * rows: the number of rows in the file
@@ -317,7 +373,50 @@ void make_board(int *arr, int rows, int cols){
         }
     }
 }
+void* play_gol_thread(void* arg){
+    //Unpack the arguments that are passed into the thread
+    struct gol_data *data;
+    data = (struct gol_data*) arg;
+    int start_row = data->start_row;
+    int end_row = data->end_row;    
+    printf("Starting Row: %d\n",start_row);
+    //could call outside of this function
+    //assume that we're focusing on rows
+    //lock
+    //use a global count variable
+    //start_row = row_info[count][0]
+    //end_row = row_info[count][0]
+    //unlock
 
+    //Process the partition of the game board assigned to this thread
+    for(int a = 0; a<data->rounds; a++){
+        for(int i = start_row; i <= end_row; i++){
+            for(int j = 0; j < data->cols; j++){
+                int live_neighbors = count_alive(data, i, j);
+                make_alive(data, i, j, live_neighbors); 
+            }
+        }
+    // replaces the current board with next board
+    int *temp = data->current;
+    data->current = data->next;
+    data->next = temp;
+    //If the output_mode is 1 then the program runs the ASCII version
+        if(data->output_mode == 1){
+            system("clear");
+            print_board(data, data->rounds);
+            usleep(SLEEP_USECS);
+        }
+        
+        //If output_mode is 2 then the program runs the animation version
+        else if(data->output_mode == 2){ 
+            update_colors(data);
+            draw_ready(data->handle);
+            usleep(SLEEP_USECS);
+        } 
+    }
+    return NULL;   
+    //pthread_exit(NULL);
+}
 
 /* the gol application main loop function:
  *  runs rounds of GOL,
@@ -335,12 +434,12 @@ void* play_gol(void* args) {
     int* temp = NULL; 
     
     //Gives each thread the number of rows it reads
-    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    printf("Before Math: %d\n",data->num_threads);
     data->rows_per_thread = data->rows / data->num_threads;
     rt = data->rows %data->num_threads;
     //LOCK
     pthread_mutex_lock(&mutex);
-    data->starting_row = sr_count;
+    data->start_row = sr_count;
     if(rt!=0){
         rt--;
         data->rows_per_thread ++;
@@ -349,7 +448,7 @@ void* play_gol(void* args) {
         sr_count++;
     }
     pthread_mutex_unlock(&mutex);
-    printf("%d\n", data->starting_row);
+    printf("%d\n", data->start_row);
     //UNLOCK
     /*
 
